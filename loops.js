b828ac535588a77dc232fb1349323720680cdb09
savyounts@gmail.com <savyounts@gmail.com>
2018-05-15 13:09:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,6 +16,12 @@ function whileLoop(n){
   return 'done'
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
+  do {
+} while (array.length > 0 && maybeTrue())
   
 }
